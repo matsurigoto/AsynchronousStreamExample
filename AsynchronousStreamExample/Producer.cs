49250e32645d090a8274e686428d472b3fc134b1
Duran Hsieh <chhsie@microsoft.com>
2020-01-21 00:25:41 +0800

    init

---
+++ AsynchronousStreamExample/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreamExample
+{
+    class EmployeeInfo 
+    {
+        public int Id { get; set; }
+    }
+
+    class Producer
+    {
+        public async IAsyncEnumerable<EmployeeInfo> GetEmployeeInfosAsync(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await Task.Delay(500);
+                yield return new EmployeeInfo(){Id = i +1};
+            }
+        }
+    }
+}
---
+++ AsynchronousStreamExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreamExample
+{
+    class Program
+    {
+        private static int ThreadId => Thread.CurrentThread.ManagedThreadId;
+
+        static async Task Main(string[] args)
+        {
+            var producer = new Producer();
+            
+            Console.WriteLine($"({ThreadId})取得員工資料");
+
+            await foreach (var employee in producer.GetEmployeeInfosAsync(20))
+            {
+                Console.WriteLine($"({ThreadId})開始接收資料: {employee.Id}");
+            }
+
+            Console.WriteLine($"({ThreadId})接收資料完畢");
+        }
+    }
+}
